f748ef8138f1dde2fd2695b63ce3d6210987ff77
SaraswathiBollu <bollusaisaraswathi@gmail.com>
2023-04-20 01:48:13 +0530

    all

--- routes/student/viewinsights.js
+++ routes/student/viewinsights.js
@@ -5,37 +5,29 @@ const bcrypt = require('bcrypt');
 const bodyParser=require('body-parser');
 var cookieParser = require('cookie-parser');
 const router = express.Router();
-const Interview  = require('../../models/interview'); 
+app.set('view engine', 'ejs');
+const path = require('path');
 
-router.get('/', (req, res) => {
+const { Interview } = require('../../models/interview'); 
 
-  // const myCookie = req.cookies.st;
-  // if (!myCookie) {
-  //     res.send("switch to TPO account");
-  // }
-  res.render('viewinsights', { message: '' })
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+router.get('/',(req,res)=>{
 
-router.post('/', async (req, res) => {
-  try{
-    console.log(req.body)
-    const username = req.body.username;
-    const name = req.body.name;
-    const jobrole = req.body.jobrole;
-    const company = req.body.company
-    const salary = req.body.salary 
-    const experiences =req.body.experiences 
-    const suggestions =req.body.suggestions
-    const interview = new Interview({name,username,company,jobrole,salary,experiences,suggestions});
-    await interview.save();
-    res.render('shareexperience',{message:"Shared Succesfully"});
-  }
-  catch(error){
-res.status(400).send(error);
-  }
-  
-  
-});
+  const myCookie = req.cookies.user;
+  if(!myCookie){
+      res.send("switch to student account");
+      return;
+    }
 
-module.exports = router;
+    Interview.find({company:req.body.company})
+        .toArray((err,documents) => {
+            if(err) throw err;
+            ejs.renderFile(path.join(__dirname,'../../views/viewinsights.ejs'),{
+                experiences:documents
+            }
+            )})       
+        
+})
+  module.exports = router;
